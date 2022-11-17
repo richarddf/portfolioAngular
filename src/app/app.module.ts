997e6acdcabcd6ae64ad10b225795c3a07fad32c
Richard Diaz Flores <richarddf@gmail.com>
2022-11-17 11:03:26 -0300

    primer commit con componentes creados en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ModalLoginComponent } from './componentes/modal-login/modal-login.component';
+import { ModalAcercadeComponent } from './componentes/modal-acercade/modal-acercade.component';
+import { ModalExperienciaComponent } from './componentes/modal-experiencia/modal-experiencia.component';
+import { ModalEducacionComponent } from './componentes/modal-educacion/modal-educacion.component';
+import { ModalHabilidadesComponent } from './componentes/modal-habilidades/modal-habilidades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AcercadeComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent,
+    ModalLoginComponent,
+    ModalAcercadeComponent,
+    ModalExperienciaComponent,
+    ModalEducacionComponent,
+    ModalHabilidadesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/modal-acercade/modal-acercade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-acercade',
+  templateUrl: './modal-acercade.component.html',
+  styleUrls: ['./modal-acercade.component.css']
+})
+export class ModalAcercadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
