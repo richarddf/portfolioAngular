4b9f53da8519c908edf82a9d13e0efb3958ff764
Richard Diaz Flores <richarddf@gmail.com>
2022-11-22 20:43:31 -0300

    hechos todos los componentes de Angular y comenzando a agregar json data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ import { ModalHabilidadesComponent } from './componentes/modal-habilidades/modal
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
