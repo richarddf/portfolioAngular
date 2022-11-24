e75658f3b223571b14f16413db438ee867815bc4
Richard Diaz Flores <richarddf@gmail.com>
2022-11-24 11:09:22 -0300

    modificación de periodo separado en dos campos fechas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { AcercadeComponent } from './componentes/acercade/acercade.component';
